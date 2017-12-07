2e2c4b7ac9456692587de93b3f71b248fd131a00
suruchi-bhatia <suruchi.bhatia@hotmail.com>
2017-12-07 21:38:32 +0100

    Added another message

--- Basics/src/main/java/se/basics/selenium/FirstSelenium.java
+++ Basics/src/main/java/se/basics/selenium/FirstSelenium.java
@@ -5,4 +5,8 @@ public class FirstSelenium {
     public static void main(String[] args) {
         System.out.println("First change on the branch");
     }
+
+    public static void simpleMessage() {
+        System.out.println("Hello ");
+    }
 }
