e54801d9c360a3805ca38487e9426dc0b1c5e14c
suruchi-bhatia <suruchi.bhatia@hotmail.com>
2017-12-11 21:56:38 +0100

    We added few comments. Added new dependency for Selenium. Added a new Java Class - Basic Selenium commands.

--- Basics/src/main/java/se/basics/selenium/FirstSelenium.java
+++ Basics/src/main/java/se/basics/selenium/FirstSelenium.java
@@ -10,3 +10,31 @@ public class FirstSelenium {
         System.out.println("Hello ");
     }
 }
+/*
+<assuming we are already have a repository created, and working on the project which is available in GIT>
+-- when we write the code
+add the files to the repository
+commit the files
+Push the files to the remote repository
+
+-- when a teammate writes the code
+Our role:
+pull the updated code from the remote repository to the local
+
+
+
+General activities:
+- write text on the webpage
+- click on links/buttons
+- select radio buttons
+- select/unselect checkboxes
+- change options from the drop-down menu
+
+
+1. Browser
+2. Exact location where to perform the action
+3. Command - action to be performed
+4. test data**
+
+
+ */
---
+++ Basics/src/main/java/se/basics/selenium/WebDriverExample.java
@@ -0,0 +1,43 @@
+package se.basics.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverExample {
+
+
+    public static void main(String[] args) {
+
+        // as the error messsage says: Set the system property for webdriver.chrome.driver with path to the exe file
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suruc\\jars\\chromedriver.exe");
+
+        // Launch the chrome browser
+        WebDriver browser = new ChromeDriver();
+
+
+        // navigate to a website
+        browser.navigate().to("http://www.google.com");
+
+        // capture the titile
+        System.out.println(browser.getTitle() + " - - - - is the title of the current webpage");
+
+        // current url of the webpage
+        System.out.println(browser.getCurrentUrl() + " - - - -is the URL");
+
+        // alternative keyword for navigation
+        browser.get("http://www.fb.com");
+        System.out.println(browser.getTitle() + "- - - - is the title of the current webpage");
+        System.out.println(browser.getCurrentUrl() + " - - - -is the URL");
+
+        // capture some informatin from the website
+        // - title of the page
+        // - url of the page
+        // -...t.ex
+        // close the browser
+        browser.close();
+// the below line will give nll pointer exception as the browser has already been closed.
+//        System.out.println(browser.getTitle());
+
+    }
+
+}
